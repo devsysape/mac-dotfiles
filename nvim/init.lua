a3bd42d98005ce4b7a4654d70577f859b13b6d54
devsysape <153757825+devsysape@users.noreply.github.com>
2024-06-24 15:31:00 -0700

    updated with customization for nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,3 +37,6 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- Customization
+require "custom"
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,6 +25,7 @@ return {
 
   {
   	"nvim-treesitter/nvim-treesitter",
+    lazy = false,
   	opts = {
   		ensure_installed = { "vim", "lua", "vimdoc", "html", "css", "elixir", "eex", "heex" },
   	},
